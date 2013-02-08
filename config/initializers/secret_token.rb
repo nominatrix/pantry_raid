2aa0dbd96e5d3c7b8c69604401c9148894f4bca7
Belynda Cianci <Belynda@Belynda-Ciancis-MacBook-Pro.local>
2013-02-07 22:47:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PantryRaid::Application.config.secret_token = '088b4d571518fbbaa1b227fda5dfe34d938fccadf467ed8ac797eaf6a262a56c767b5c33ac78fcdf306332ba12c7fb941f5b885652e71bfa385b19634129efa3'
